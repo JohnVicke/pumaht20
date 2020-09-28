0ff3118fc0edc29f1baf2c9b72a95d5e2ea814db
Viktor Malmedal <viktormalmedal@gmail.com>
2020-09-28 11:32:25 +0200

    #2 adds state managment

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { values } from "mobx";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { store } from "./store";
 
 export default function App() {
+  useEffect(() => {
+    setLocalStore(store);
+  }, []);
+
+  const [id, setId] = useState(0);
+  const [localStore, setLocalStore] = useState(store);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Hello world"
+        onPress={() => {
+          setId(id + 1);
+          store.addTodo(id, "hello");
+        }}
+      />
+
+      {values(localStore.todos).map((todo: any) => (
+        <Text>{todo.name}</Text>
+      ))}
     </View>
   );
 }
@@ -14,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
